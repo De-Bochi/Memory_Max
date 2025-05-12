f36032c59251c37615cf5916d6f56cfe16aa6f72
MachadoDias <gabrielgabriel181007@gmail.com>
2025-05-12 20:19:32 -0300

    correção jogo.c

--- Core/Src/jogo.c
+++ Core/Src/jogo.c
@@ -1,5 +1,4 @@
 #include "st7789\st7789.h"
-#include "stdbool.h"
 void DrawCard(uint16_t x, uint16_t y, uint16_t color, const uint64_t* card, uint16_t bgcolor){
 	for(int j = 0; j<50; j++){
 		for(int i = 0; i<50; i++){
